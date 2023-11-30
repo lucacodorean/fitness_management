a71e3421bfa5e248c4228fdf3ddb5c506f9f5915
Luca <47753569+lucacodorean@users.noreply.github.com>
2023-11-30 18:10:36 +0200

    fix: Rating updates now as intended.

--- database/triggers/updateEmployeeRating.sql
+++ database/triggers/updateEmployeeRating.sql
@@ -3,7 +3,9 @@ USE fitness_management_database;
 
 CREATE TRIGGER update_employee_rating
     AFTER INSERT ON feedback
-    FOR EACH ROW UPDATE employee
+    FOR EACH ROW
        UPDATE employee 
-            SET rating = rating + (SELECT rating FROM feedback ORDER BY id DESC LIMIT 1) 
+            SET rating = (SELECT AVG(rating) 
+						  FROM feedback 
+						  WHERE employee_id = (SELECT employee_id FROM feedback ORDER BY id DESC LIMIT 1)) 
             WHERE id = (SELECT employee_id FROM feedback ORDER BY id DESC LIMIT 1);
