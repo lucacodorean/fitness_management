ef0979c9f9798cb72645a31c213e722c1aa74f1e
Luca <47753569+lucacodorean@users.noreply.github.com>
2023-12-05 16:37:32 +0200

    New modification to LoginPage.

--- src/frontend/LoginPage.java
+++ src/frontend/LoginPage.java
@@ -16,21 +16,31 @@ public class LoginPage implements ActionListener{
         JPanel panel = new JPanel();
         panel.setBorder(BorderFactory.createEmptyBorder(30, 30, 10, 30));
         panel.setLayout(new GridLayout(0, 1));
-
-        panel.add(new JLabel("Welcome to your fitness app!"));
+        panel.add(new JLabel("Welcome to your fitness app!"), BorderLayout.CENTER);
 
         JButton loginButton = new JButton("Login"); loginButton.addActionListener(this);
 
-        panel.add(emailTextField, BorderLayout.CENTER);
-        panel.add(passwordTextField, BorderLayout.CENTER);
+        emailTextField.setPreferredSize(new Dimension(200,25));
+        passwordTextField.setPreferredSize(new Dimension(200,25));
+
+        JPanel panelEmail = new JPanel();
+        panelEmail.add(new JLabel("Email:   "), BorderLayout.PAGE_START);
+        panelEmail.add(emailTextField);
+
+        JPanel panelPassword = new JPanel();
+        panelPassword.add(new JLabel("Password: "), BorderLayout.PAGE_START);
+        panelPassword.add(passwordTextField);
+
+        panel.add(panelEmail, BorderLayout.CENTER);
+        panel.add(panelPassword, BorderLayout.CENTER);
         panel.add(loginButton, BorderLayout.CENTER);
 
 
         JFrame frame = new JFrame();
+        frame.setSize(new Dimension(400, 400));
         frame.add(panel, BorderLayout.CENTER);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setTitle("Login page");
-        frame.pack();
         frame.setVisible(true);
     }
 
